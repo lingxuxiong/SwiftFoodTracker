9093b0a8f3634b74e3aeef70cdf41f695e44c1f5
Neil Ling <neil.ling@aylanetworks.com>
2018-10-11 15:52:18 +0800

    Initial Commit

---
+++ foodtracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  foodtracker
+//
+//  Created by Neil Ling on 2018/10/11.
+//  Copyright © 2018 Neil Ling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
