1c04a4cce090a1781bf5710ec51effc9d47418db
Neil Ling <neil.ling@aylanetworks.com>
2018-12-20 15:35:24 +0800

    add spacings and @IBDesignable declaration to the rating control

--- foodtracker/RatingControl.swift
+++ foodtracker/RatingControl.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
+    
+    // MARK: Properties
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0
     
     // MARK: Initialization
     override init(frame: CGRect) {
@@ -28,19 +33,24 @@ class RatingControl: UIStackView {
     
     // MARK: private methods
     private func setupButtons() {
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
-        
-        // add constraints
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
-        
-        // setup the button action
-        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
-        
-        // add the button to the stack
-        addArrangedSubview(button)
+        // create the buttons, five in total
+        for _ in 0..<5 {
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            
+            // add constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            
+            // setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            // add the button to the stack
+            addArrangedSubview(button)
+            
+            // add the new button to the rating buttons array
+            ratingButtons.append(button)
+        }
     }
-
 }
