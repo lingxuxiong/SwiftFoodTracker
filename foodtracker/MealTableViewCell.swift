3baafa5b02dc2fcda72db787d617863400c45e4c
Neil Ling <neil.ling@aylanetworks.com>
2018-12-25 10:41:38 +0800

    add table view cell to represent a meal

---
+++ foodtracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  foodtracker
+//
+//  Created by Neil Ling on 2018/12/24.
+//  Copyright © 2018 Neil Ling. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
