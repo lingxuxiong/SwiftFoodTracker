29ad39d6a13ef944b49d6ef99eec4ade814922b1
Neil Ling <neil.ling@aylanetworks.com>
2018-12-26 15:55:58 +0800

    make Meal inherit from NSObject and implement NSCoding to get prepared for persistence

--- foodtracker/Meal.swift
+++ foodtracker/Meal.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import os.log
 
-class Meal {
+class Meal: NSObject, NSCoding {
     
     // MARK: properties
     var name: String
     var photo: UIImage?
     var rating: Int
+    
+    // MARK: Arching paths
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first
+    static let ArchiveURL = DocumentsDirectory?.appendingPathComponent("meals")
+    
+    // MARK: Types
+    struct PropertyKey {
+        static let name = "Name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
     
     // MARK: initialization
     init?(name: String, photo: UIImage?, rating: Int) {
@@ -28,5 +40,31 @@ class Meal {
         self.name = name
         self.photo = photo
         self.rating = rating
+    }
+    
+    // MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+    }
+    
+    // The convenience modifier means that this is a secondary initializer,
+    // and that it must call a designated initializer from the same class.
+    required convenience init?(coder aDecoder: NSCoder) {
+        // The name is required. If we cannot decode a name string, the initializer should fail.
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to device meal name.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        // photo is optional, so just use conditional cast.
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        
+        // no need to downcast to Int and there is no optional to unwrap?
+        let rating = aDecoder.decodeObject(forKey: PropertyKey.rating) as? Int
+        
+        // must call designated initializer.
+        self.init(name: name, photo: photo, rating: rating ?? 0)
     }
 }
